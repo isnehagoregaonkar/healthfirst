e4bc210350a2fd560aa35e57ea56c1addabc2e76
Sneha Goregaonkar <goregaonkarsneha98@gmail.com>
2026-04-10 18:39:36 +0400

    session

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,9 @@
 /* eslint-env jest */
 
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest').default,
+);
+
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');
   Reanimated.default.call = () => {};
