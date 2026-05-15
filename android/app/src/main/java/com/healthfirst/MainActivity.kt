491e43dbb6fc52cf74eb47dfa5aad55d29d6b5f4
isnehagoregaonkar <goregaonkarsneha98@gmail.com>
2026-05-15 14:40:47 +0400

    apk build and fixes

--- android/app/src/main/java/com/healthfirst/MainActivity.kt
+++ android/app/src/main/java/com/healthfirst/MainActivity.kt
@@ -1,6 +1,6 @@
-import android.os.Bundle
 package com.healthfirst
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,19 +11,18 @@ class MainActivity : ReactActivity() {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * rendering of the main component.
    */
   override fun getMainComponentName(): String = "HealthFirst"
 
   override fun onCreate(savedInstanceState: Bundle?) {
-   super.onCreate(savedInstanceState)
-   // In order to handle permission contract results, we need to set the permission delegate.
-  HealthConnectPermissionDelegate.setPermissionDelegate(this)
-}
+    super.onCreate(savedInstanceState)
+    HealthConnectPermissionDelegate.setPermissionDelegate(this)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * which allows you to enable New Architecture with a single boolean flag [fabricEnabled].
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
--- android/app/src/main/java/com/healthfirst/PermissionRationaleActivity.kt
+++ android/app/src/main/java/com/healthfirst/PermissionRationaleActivity.kt
@@ -1,4 +1,4 @@
-package com.healthconnectexample
+package com.healthfirst
 
 import android.os.Bundle
 import android.webkit.WebResourceRequest
@@ -6,7 +6,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
-class PermissionsRationaleActivity: AppCompatActivity() {
+class PermissionsRationaleActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
